087be344b87991eccff5e591719d56311a22f29d
utkarsh <r.utkarsh.0010@gmail.com>
2024-05-25 19:43:06 +0530

    move code to main file

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { chat, config, login, read, vision } from "@/commands";
+import { getInfo } from "@/utils";
+import { Command } from "commander";
+
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+
+(async () => {
+  const packageInfo = await getInfo();
+
+  const program = new Command()
+    .name("rag-cli")
+    .description("An AI tool to help you talk with your documents.")
+    .version(
+      packageInfo.version || "1.0.0",
+      "-v, --version",
+      "display version number"
+    );
+
+  program
+    .addCommand(login)
+    .addCommand(config)
+    .addCommand(chat)
+    .addCommand(read)
+    .addCommand(vision);
+
+  program.parse();
+})();
